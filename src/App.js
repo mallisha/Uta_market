eaf872778009b09d482dfa9c2c404963ddaebdba
malli <mallipatkar1@gmail.com>
2023-11-09 16:34:38 -0600

    commit m

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './Components/home';
+import Buy from './Components/HomePage';
 import LoginForm from './Components/LoginForm';
 import Register from './Components/Register';
 import Sellform from './Components/Sellform';
@@ -17,6 +18,7 @@ function App() {
                     <Route path="/sell" element={<Sellform />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/profileedit" element={<ProfileEdit />} />
+                    <Route path="/buy" element={<Buy />} />
                     <Route path="/" exact element={<HomePage />} />
                     <Route path="/Myprofile" element={<Myprofile />} />
                     {/* Add other routes as needed */}
