3c148873ebe6e9be03852e2f65dca1a67952b87a
harshita2319 <harshita.dhingra.2319@gmail.com>
2023-11-14 00:50:22 -0600

    .

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useUser } from "../UserContext";
-import "./Logout.css";
+//import "./Logout.css";
 
 function Logout() {
   const { updateUser } = useUser();
